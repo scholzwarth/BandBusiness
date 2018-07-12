04c4cbc57c18c3be39acf1e3e2c0c3235ca72958
FirstName LastName <email@address.here>
2018-07-12 08:05:43 -0400

    Added and organized files - includes all old work

---
+++ BandBusiness/BandBusiness-I1/BandBusiness-I1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BandBusiness_I1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ First-Trys/WebsiteStuff/scripts/myScripts.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    $('#btnHost').click(function () {
+        $('#modalAccess').addClass('hidden');
+    }); 
+    $('#btnBackHome').click(function () {
+        $('#modalAccess').removeClass('hidden');
+    });
+
+});
+
+function showModal(id) {
+    $('.modal').modal('hide');
+    $('#' + id).modal('show');
+};
+
